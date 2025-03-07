22872bca9f77fb0b48926ab19f6595a2a5230827
hungvu <hungns126@gmail.com>
2025-03-07 22:10:58 +0700

    fix text

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="w-full max-w-6xl mx-auto p-3 sm:p-6">
     <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-center text-gray-800 mb-4 sm:mb-8">Hexdump to Command Converter for Kindle</h1>
-    <p className="text-center text-green-600 mb-4 font-bold">Develop by Cee</p>
+    <p className="text-center text-green-600 mb-4 font-bold">Developed by Cee</p>
     {/* Controls Section */}
     <Main />
 
